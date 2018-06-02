e5f4c5ee3223363e192a9d39f1648fe33377f58c
markszabo <markszabo@users.noreply.github.com>
2018-06-02 09:52:45 +0200

    [Virus cleanup] page.tpl updated with changes

--- sites/all/themes/magazeen_lite/templates/page.tpl.php
+++ sites/all/themes/magazeen_lite/templates/page.tpl.php
@@ -5,13 +5,13 @@
     <div id="logo">
 		<?php if ($logo): ?>
         <a href="<?php print check_url($front_page); ?>" title="<?php print t('Home'); ?>">
-	
-	<img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /> 
 
-	
+	<img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+
+
 	</a>
         <?php endif; ?>
- 
+
         <?php if ($site_name || $site_slogan): ?>
         <div class="clearfix">
         <?php if ($site_name): ?>
@@ -26,7 +26,7 @@
     <div id="searchform-header">
      	<?php print render($page['search_area']); ?>
    	</div>
-  </div> <!-- /header-container -->    
+  </div> <!-- /header-container -->
 </div> <!-- /header -->
 
 <!-- ______________________ NAVIGATION _______________________ -->
@@ -34,8 +34,21 @@
 <?php if (!empty($main_menu)): ?>
   <div id="navigation"><!-- navigation -->
     <div class="container clearfix">
+    <?php if('multilevel_menu'){
+      // echo "<pre>";
+      // var_dump(menu_build_tree('main-menu'));
+      $children = menu_build_tree('main-menu');
+      $menu_render = menu_tree_output($children);
+      $menu_render = json_decode(str_replace('leaf','',json_encode($menu_render)),true);
+      $menu_render['#attributes']['class']=array('main-menu','links');
+      $r = drupal_render($menu_render);
+      echo str_replace('<ul class="','<ul class="links main-menu',$r);
+      echo '<a title="Pibe hírek RSS" class="rss" href="/news.xml">Feliratkozás</a>';
+      echo '<script>jQuery(function($){$("#navigation a.active").parent().addClass("active");});</script>';
+      }else{?>
       <?php print theme('links', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'main-menu')))); ?>
-      <a title="Subscribe to magazeen RSS" class="rss" href="">Subscribe</a>
+      <a title="Pibe hírek RSS" class="rss" href="/news.xml">Feliratkozás</a>
+      <?php } ?>
     </div> <!-- /navigation-container -->
   </div> <!-- /navigation -->
 <?php endif;?>
@@ -72,46 +85,46 @@
 <div id="main" class="clearfix">
   <div class="container clearfix">
     <div id="sidebar" class="left">
-      <?php print render($page['sidebar_second']); ?> 
+      <?php print render($page['sidebar_second']); ?>
     </div><!-- /right -->
-    
+
     <div class="main">
-		<?php if ($breadcrumb): print $breadcrumb; endif; ?>
-        
+		<?php //if ($breadcrumb): print $breadcrumb; endif; ?>
+
 		<?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
-    
+
         <?php if ($messages): ?>
         <div id="console" class="clearfix">
         <?php print $messages; ?>
         </div>
         <?php endif; ?>
-    
+
         <?php if ($page['help']): ?>
         <div id="help">
         <?php print render($page['help']); ?>
         </div>
         <?php endif; ?>
-        
+
         <?php if ($action_links): ?>
         <ul class="action-links">
         <?php print render($action_links); ?>
         </ul>
         <?php endif; ?>
-        
+
         <?php if ($tabs): ?><?php print render($tabs); ?><?php endif; ?>
-        
+
         <?php print render($title_prefix); ?>
         <?php if ($title): ?>
         <h1 class="node-title"><?php print $title ?></h1>
         <?php endif; ?>
         <?php print render($title_suffix); ?>
-        
+
         <?php print render($page['content']); ?>
-        
+
         <?php print $feed_icons; ?>
         <div id="back-to-top0"><a href="#top"><?php print(t('Back to top of the page'));?></a></div>
     </div><!-- /main -->
-    
+
   </div><!-- /main-container -->
 </div><!-- /main -->
 
